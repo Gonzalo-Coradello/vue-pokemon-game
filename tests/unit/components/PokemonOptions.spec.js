9f1856523255ce65222f13d0bc0cc44d1d992b6d
Coradello <gonzalo.coradello@accenture.com>
2023-12-20 12:05:17 -0300

    add unit tests to PokemonOptions component

---
+++ tests/unit/components/PokemonOptions.spec.js
@@ -0,0 +1,50 @@
+import { describe, test, expect, beforeEach } from 'vitest'
+import {shallowMount} from '@vue/test-utils'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+import { pokemons } from '../mocks/pokemons.mock'
+
+describe('PokemonOptions component', () => {
+
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallowMount(PokemonOptions, {
+            props: {
+                pokemons
+            }
+        })
+    })
+
+    test('debe hacer match con el snapshot', () => {
+        expect(wrapper.html()).toMatchSnapshot()
+    })
+    
+    test('debe mostrar las cuatro opciones correctamente', () => {
+        const liTags = wrapper.findAll('li')
+
+        expect(liTags.length).toBe(4)
+        expect(liTags[0].text()).toBe(pokemons[0].name)
+        expect(liTags[1].text()).toBe(pokemons[1].name)
+        expect(liTags[2].text()).toBe(pokemons[2].name)
+        expect(liTags[3].text()).toBe(pokemons[3].name)
+    })
+
+    test('debe emitir "selection" con sus respectivos parámetros al hacer click', () => {
+        const [li1, li2, li3, li4] = wrapper.findAll('li')
+
+        li1.trigger('click')
+        li2.trigger('click')
+        li3.trigger('click')
+        li4.trigger('click')
+
+        expect(wrapper.emitted('selection').length).toBe(4)
+        expect(wrapper.emitted('selection')[0]).toEqual([1])
+        expect(wrapper.emitted('selection')[1]).toEqual([2])
+        expect(wrapper.emitted('selection')[2]).toEqual([3])
+        expect(wrapper.emitted('selection')[3]).toEqual([4])
+    })
+
+    test('', () => {
+
+    })
+})
